93ee63a5eb4c6aecf985194b30e5dfde30db57f1
Yousef Almesaeed <yousefalm@Outlook.com>
2024-11-03 14:42:36 +0300

    finish task

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int [] temperatures = {12, 43, 23, 42, 12};
+
+        temperatures[2] = 0;
+
+        for (int temp : temperatures) {
+            System.out.println(temp);
+
+        }
+
+
+
+
+
+    }
+}
