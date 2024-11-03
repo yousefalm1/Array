dab665dc82a1fae24495313b8f31d45373c85d4b
Yousef Almesaeed <yousefalm@Outlook.com>
2024-11-03 19:22:32 +0300

    Finished task

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -5,11 +7,39 @@ public class Main {
         int [] temperatures = {12, 43, 23, 42, 12};
 
         temperatures[2] = 0;
+//
+//        for (int temp : temperatures) {
+//            System.out.println(temp);
+//
+//        }
 
-        for (int temp : temperatures) {
-            System.out.println(temp);
 
+
+
+        ArrayList<String> songs = new ArrayList<>();
+
+        songs.add("ABC");
+        songs.add("I love you");
+        songs.add("Happy Birthday");
+        songs.add("Twinkle Twinkle");
+        songs.add("Etse betse(spider)");
+
+
+        songs.remove("Happy Birthday");
+
+        for (String song : songs)  {
+            System.out.println(song);
         }
+
+
+
+
+
+
+
+
+
+
 
 
 
